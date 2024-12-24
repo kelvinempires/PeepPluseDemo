386d7bdc642ccbd843616576679f32ffb530b3dc
kelvinempire <kelvinewurum@gmail.com>
2024-12-24 17:34:45 +0100

    i used cross-env package in setting environment variables across my different platforms

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser"; // Import cookie-parser
@@ -19,9 +20,10 @@ cloudinary.config({
 });
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000;  
+const __dirname = path.resolve();
 
-app.use(express.json({limit:"5mb"}));//to parse req.body,limit should not be too large to prevent DOS
+app.use(express.json({ limit: "5mb" })); //to parse req.body,limit should not be too large to prevent DOS
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser()); // Use cookie-parser
 
@@ -30,7 +32,12 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
-
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
+  app.get("*",(req, res)=>{
+    res.sendFile(path.resolve(__dirname, "frontend","dist", "index.html"))
+  })
+}
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
   connectMongoDB();
